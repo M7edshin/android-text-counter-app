fb6d97a18bda31d336904e69b71edfcff7bcc4cb
M7edshin <M7edshin@hotmail.com>
2017-07-20 14:48:26 +0100

    Menu option added Layout changes

--- app/src/main/java/shahin/textcounter/CompareTwo.java
+++ app/src/main/java/shahin/textcounter/CompareTwo.java
@@ -3,10 +3,14 @@ package shahin.textcounter;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -221,6 +225,63 @@ public class CompareTwo extends AppCompatActivity {
         }else{
             Toast.makeText(getApplicationContext(), R.string.str_toast_auto_text_off, Toast.LENGTH_SHORT).show();
             txtBottomString.removeTextChangedListener(textWatcherBottomString);
+        }
+    }
+
+
+    /**
+     * Methods relate to the Menu option
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_options items for use in the action bar
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnMyApps:
+                String urlWebsite = "https://play.google.com/store/apps/developer?id=Mohamed+Shahin";
+                Uri uriWebpage = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriWebpage);
+                if (intentWebsite.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intentWebsite);
+                }
+                return true;
+            case R.id.btnReport:
+                sendEmail("Report a bug");
+                return true;
+            case R.id.btnSuggest:
+                sendEmail("Suggest a feature or recommendation");
+                return true;
+            case R.id.btnExit:
+                finish();
+                System.exit(0);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Sending email to the developer based on the user
+     *
+     * @param text
+     */
+    private void sendEmail(String text) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"M7edshin@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, text);
+        i.putExtra(Intent.EXTRA_TEXT, "Your Message");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(CompareTwo.this, "There are no email clients or apps installed on your device.", Toast.LENGTH_SHORT).show();
         }
     }
 }
